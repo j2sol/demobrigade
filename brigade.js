035b0e5b88ec6098e1a2c8bce4019a2dfd8fa9c3
Jesse Keating <jkeating@j2solutions.net>
2017-11-06 15:58:27 -0800

    Remove space in job name

--- brigade.js
+++ brigade.js
@@ -1,7 +1,7 @@
 const { events, Job } = require('brigadier')
 
 events.on("push", (brigadeEvent, project) => {
-    var job = new Job("Demo Job", "alpine:3.4")
+    var job = new Job("DemoJob", "alpine:3.4")
     job.tasks = [
         "echo Line 1",
         "echo Line 2"
