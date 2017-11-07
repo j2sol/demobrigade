c7cb6bcdef7926c56152a0ccaa3fb49f2caf20c9
jkeating <jlk@derpops.bike>
2017-11-06 16:38:03 -0800

    handle pull request with no secrets

--- brigade.js
+++ brigade.js
@@ -1,14 +1,10 @@
 const { events, Job } = require('brigadier')
 
-events.on("push", (brigadeEvent, project) => {
+events.on("pull_request", (brigadeEvent, project) => {
     var job = new Job("demojob", "alpine:3.4")
-    job.env = {
-        "leakme": project.secrets.leakme
-    }
     job.tasks = [
         "echo Line 1",
-        "echo Line 2",
-        "echo $leakme"
+        "echo Line 2"
     ]
 
     job.run()
