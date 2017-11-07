9cc624a0530359e5b33cbf32f486f4bd04d6ee03
Jesse Keating <jkeating@j2solutions.net>
2017-11-06 16:06:00 -0800

    Make use of secret
    This logs it to k8s, could expose the secret

--- brigade.js
+++ brigade.js
@@ -2,9 +2,13 @@ const { events, Job } = require('brigadier')
 
 events.on("push", (brigadeEvent, project) => {
     var job = new Job("demojob", "alpine:3.4")
+    job.env = {
+        "leakme": project.secrets.leakme
+    }
     job.tasks = [
         "echo Line 1",
         "echo Line 2"
+        "echo $leakme"
     ]
 
     job.run()
