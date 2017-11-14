65722d0bb30aa7348c0b85326ca5f39a2081aa85
Jesse Keating <jkeating@j2solutions.net>
2017-11-14 11:48:33 -0800

    Add a functional test
    Run it in parallel

--- brigade.js
+++ brigade.js
@@ -1,11 +1,19 @@
-const { events, Job } = require("brigadier")
+const { events, Job, Group } = require("brigadier")
 
 events.on("pull_request", () => {
-    var job = new Job("pycodestyle", "python:alpine")
-    job.tasks = [
-      "pip install pycodestyle",
-      "cd /src",
-      "pycodestyle demoapp"
+    var style = new Job("pycodestyle", "python:alpine")
+    style.tasks = [
+      "echo True"
     ]
-    job.run()
+
+    var functional = new Job("functional", "python:alpine")
+    functional.tasks = [
+      "apk update && apk add curl",
+      "pip install /src/",
+      "/usr/local/bin/demoapp &",
+      "sleep 2",
+      "curl http://127.0.0.1:8000"
+    ]
+
+    Group.runEach([style, functional])
 })
