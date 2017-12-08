d5fd001ba07008588f010040b196a09aef34af30
jkeating <jlk@derpops.bike>
2017-12-08 15:43:14 -0600

    Create the brigade.js
    Do a thing.

---
+++ brigade.js
@@ -0,0 +1,29 @@
+const { events, Job } = require('brigadier')
+
+events.on("push", (brigadeEvent, project) => {
+    var job = new Job("demojob", "alpine:3.4")
+    job.env = {
+        "leakme": project.secrets.leakme
+    }
+    job.tasks = [
+        "echo Line 1",
+        "echo Line 2",
+        "echo $leakme"
+    ]
+
+    job.run()
+})
+
+events.on("pull_request:labeled", (brigadeEvent, project) => {
+    var job = new Job("pulljob", "alpine:3.4")
+    job.env = {
+        "leakme": project.secrets.leakme
+    }
+    job.tasks = [
+        "echo Line 1",
+        "echo Line 2",
+        "echo $leakme"
+    ]
+
+    job.run()
+})
